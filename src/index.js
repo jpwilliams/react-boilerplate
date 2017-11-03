acaa6b381230750f05e1dfc677170058ee189ce5
Jack Williams <jpwilliamsphotography@gmail.com>
2017-11-03 12:07:06 +0000

    🐛 Don't import non-existent firebase lib

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 
-import './firebase'
 import Root from './components/Root/index'
 import './assets/styles/index.scss'
 
